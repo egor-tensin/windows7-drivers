781b326193daaca434e0b536ec1e9d73e97d1fa9
Egor Tensin <Egor.Tensin@gmail.com>
2015-05-06 23:51:55 +0300

    add the '\date' field to source files

--- src/sysenter/main.c
+++ src/sysenter/main.c
@@ -1,6 +1,7 @@
 /**
  * \file
  * \author Egor Tensin <Egor.Tensin@gmail.com>
+ * \date 2015
  * \copyright This file is licensed under the terms of the MIT License.
  *            See LICENSE.txt for details.
  */
