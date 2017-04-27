1fd20a258de4b70be9455d659e7c11e35f99c26b
Egor Tensin <Egor.Tensin@gmail.com>
2017-04-27 19:44:27 +0300

    minimal: update

--- km/src/minimal/main.c
+++ km/src/minimal/main.c
@@ -1,12 +1,23 @@
+/*
+ * Copyright (c) 2017 Egor Tensin <Egor.Tensin@gmail.com>
+ * This file is part of the "Windows 7 drivers" project.
+ * For details, see https://github.com/egor-tensin/windows7-drivers.
+ * Distributed under the MIT License.
+ */
+
 #include <ntddk.h>
 
 static void on_driver_unload(DRIVER_OBJECT *driver_object)
-{ }
+{
+    UNREFERENCED_PARAMETER(driver_object);
+}
 
 NTSTATUS DriverEntry(
     DRIVER_OBJECT *driver_object,
     UNICODE_STRING *registry_path)
 {
+    UNREFERENCED_PARAMETER(registry_path);
+
     driver_object->DriverUnload = on_driver_unload;
     return STATUS_SUCCESS;
 }
